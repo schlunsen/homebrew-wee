706757cd27ca6306e404d0f55c9297656805126d
Rasmus Schlünsen <rasmus@schlunsen.com>
2026-04-05 14:59:05 +0200

    Update formula for v1.9.0

--- Formula/wee.rb
+++ Formula/wee.rb
@@ -1,25 +1,25 @@
 class Wee < Formula
   desc "Wee - Control center and wrapper for Claude Code"
   homepage "https://github.com/schlunsen/wee"
-  version "1.8.0"
+  version "1.9.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/schlunsen/wee/releases/download/v1.8.0/wee-darwin-arm64.tar.gz"
-      sha256 "4d6eb3cdf927f05e6842842b714e295a9dd8db641b28b1d586ef5e975ee8070f"
+      url "https://github.com/schlunsen/wee/releases/download/v1.9.0/wee-darwin-arm64.tar.gz"
+      sha256 "20907d1d5c99a3da426b3d1375a09d428b70501fa1933ce2c45139ed8f2e961f"
     else
-      url "https://github.com/schlunsen/wee/releases/download/v1.8.0/wee-darwin-amd64.tar.gz"
-      sha256 "3fccbf96f315a4ab1ce2b0a238f9caf198c77f128eca379376db22ac16815ada"
+      url "https://github.com/schlunsen/wee/releases/download/v1.9.0/wee-darwin-amd64.tar.gz"
+      sha256 "3605d7ec737265d7d6e93e9235d8bac3c1b8dd697161395f83fc5e429dc6c0a9"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/schlunsen/wee/releases/download/v1.8.0/wee-linux-arm64"
-      sha256 "b3bcb7a55665fab6ebf97dbf9c64d15e5519d2da80acbfa60d6a9fe381d9d8d5"
+      url "https://github.com/schlunsen/wee/releases/download/v1.9.0/wee-linux-arm64"
+      sha256 "b1bf9fc04951440daff1891e62dc7d69b041ef1719a3fe2a36782a06c38ff085"
     else
-      url "https://github.com/schlunsen/wee/releases/download/v1.8.0/wee-linux-amd64"
-      sha256 "62930d2ddb71fdb43cbb6cc2a7fe90987acb4926bfd03ad9f2a9911b89cbcd92"
+      url "https://github.com/schlunsen/wee/releases/download/v1.9.0/wee-linux-amd64"
+      sha256 "6bc48c1874f42b9717399ce57811c089273a19f58e93af9ba52336ff4abf9e90"
     end
   end
 
